a9990d4d4f20ad79e4e408189f702419b80875fe
David Guzman <davidguzmaninc@gmail.com>
2020-12-14 14:13:15 -0500

    Created the homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/header/Header';
+import Home from './components/home/Home';
 import Footer from './components/footer/Footer';
 import './App.css';
 
@@ -7,7 +8,7 @@ const App = () => {
     return (
         <div className='App'>
             <Header />
-
+            <Home />
             <Footer />
         </div>
     );
---
+++ client/src/components/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <section className='Home'>
+            <div>
+                <h1>Story 1</h1>
+            </div>
+        </section>
+    );
+};
+
+export default Home;
