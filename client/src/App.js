eaa8fc4aaefb22d1596fd292cf966bfc5259871c
David Guzman <davidguzmaninc@gmail.com>
2020-12-14 13:54:52 -0500

    Added a footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 import './App.css';
 
 const App = () => {
     return (
         <div className='App'>
             <Header />
+
+            <Footer />
         </div>
     );
 };
---
+++ client/src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => (
+    <footer className='Footer'>
+        <p className='footer-text'>
+            Another Project by{' '}
+            <a href='#' target='_blank'>
+                CNSplit
+            </a>
+        </p>
+    </footer>
+);
+
+export default Footer;
