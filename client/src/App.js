24fcc72b825eb692ab56443aee316d2056ea6b60
David Guzman <davidguzmaninc@gmail.com>
2020-12-14 13:42:02 -0500

    Added a Header Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './components/header/Header';
 import './App.css';
 
 const App = () => {
     return (
         <div className='App'>
-            <h1>Cardality</h1>
+            <Header />
         </div>
     );
 };
---
+++ client/src/components/header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+    <>
+        <h1 className='logo'>Cardality</h1>
+    </>
+);
+
+export default Header;
