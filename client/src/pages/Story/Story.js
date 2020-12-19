6caca99f330720a3bb43786ce35e425a6b5bbdce
Paul <webdevpaulmason@gmail.com>
2020-12-18 16:27:20 -0800

    (PM) Cleanup Redux Testing Code. Get Story.js ready for Backend route for card loading.

--- client/src/pages/Story/Story.js
+++ client/src/pages/Story/Story.js
@@ -1,49 +1,68 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import axios from "axios";
 
-const Story = ({ testString, currentStoryId, currentPage }) => {
+const Story = ({ currentStoryId, currentPage }) => {
+  useEffect(() => {
+    //temp route to grab id, later this ID will be accessed from the Redux State "currentStoryId", which will be set from the home page when you click on the available stories.
+    axios
+      .get(`/home`)
+      .then((res) => {
+        console.log("STORY ID TESTER: ", res.data[0]);
+        setStory(res.data[0]);
+      })
+      .catch((err) => console.log(err.message));
+  }, []);
+
   const [story, setStory] = useState([]);
   //Axios call to pull the right story and page number, changing this if needed once backend is setup. Story ID should be passed into the redux currentStoryId state from the home page component. currentPage defaults to 1.
-  axios
-    .get(`/story?id=${currentStoryId}&page=${currentPage}`)
-    .then((res) => {
-      console.log(res);
-      setStory(res);
-    })
-    .catch((err) => console.log(err.message));
-  // TEMPORARY DATA UNTIL BACKEND IS SETUP
-  const CARDDATA = [
-    { cardText: "lorem lorem lorem lorem" },
-    { cardText: "lorem lorem lorem lorem" },
-    { cardText: "lorem lorem lorem lorem" },
-    { cardText: "lorem lorem lorem lorem" },
-    { cardText: "lorem lorem lorem lorem" },
-    { cardText: "lorem lorem lorem lorem" },
-    { cardText: "lorem lorem lorem lorem" },
-    { cardText: "lorem lorem lorem lorem" },
-    { cardText: "lorem lorem lorem lorem" },
-    { cardText: "lorem lorem lorem lorem" },
-  ];
-  //CREATE A RENDER USING MAP AFTER THE AXIOS CALL RETURNS THE ARRAY OF CARDS.
-  //LOAD 10 at a time
+  // axios
+  //   .get(`/story?id=${currentStoryId}&page=${currentPage}`)
+  //   .then((res) => {
+  //     console.log(res);
+  //     setStory(res);
+  //   })
+  //   .catch((err) => console.log(err.message));
+  // TEMPORARY DATA UNTIL BACKEND IS SETUP, use "story" instead of "TEMPSTORY"
+  const TEMPSTORY = {
+    id: 1,
+    name: "Story 1",
+    cards: [
+      {
+        text: "Lorem lorem lorem lorem",
+      },
+      {
+        text: "Lorem lorem lorem lorem",
+      },
+      {
+        text: "Lorem lorem lorem lorem",
+      },
+      {
+        text: "Lorem lorem lorem lorem",
+      },
+      {
+        text: "Lorem lorem lorem lorem",
+      },
+      {
+        text: "Lorem lorem lorem lorem",
+      },
+      {
+        text: "Lorem lorem lorem lorem",
+      },
+    ],
+  };
+  //LOAD 10 cards at a time
   const handleClick = () => {
     //Logic for handling loading more feature
   };
   return (
     <div className="Story">
-      <h1>I'M THE STORY PAGE</h1>
-      <h2>REDUX TEST: {testString}</h2>
+      <h1>{TEMPSTORY.name}</h1>
       <div>
         <ul>
-          {CARDDATA.map((card) => {
-            return <li>{card.cardText}</li>;
+          {TEMPSTORY.cards.map((card) => {
+            return <li>{card.text}</li>;
           })}
-        </ul>
-        <ul>
-          {/* {story.map((card) => {
-            return <li>{card.cardText}</li>;
-          })} */}
         </ul>
         <button onClick={handleClick}>LOAD MORE</button>
       </div>
@@ -52,7 +71,6 @@ const Story = ({ testString, currentStoryId, currentPage }) => {
 };
 
 const mapStateToProps = (state) => ({
-  testString: state.story.testString,
   currentStoryId: state.story.currentStoryId,
   currentPage: state.story.currentPage,
 });
