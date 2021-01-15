97fc2332cd769d2f8c982c6cf67a2d486c3c9975
David Guzman <davidguzmaninc@gmail.com>
2021-01-14 23:55:07 -0500

    Added the error

---
+++ client/src/components/Error/Error.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error = () => {
+    return (
+        <div className="error-wrapper">
+            <h4>Oh no! The server blew up!</h4>
+        </div>
+    )
+}
+
+export default Error
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,58 +1,70 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import { connect } from "react-redux";
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { connect } from 'react-redux';
 
+import { setStoryName, setStoryId } from '../../redux/story/story.actions';
 
-import { setStoryName, setStoryId } from "../../redux/story/story.actions";
+import Error from '../../components/Error/Error'
 
-import "./Home.scss";
+import './Home.scss';
 
 const Home = ({ setStoryId, setStoryName }) => {
-  const [storyArr, setStoryArr] = useState([]);
+    const [storyArr, setStoryArr] = useState([]);
+    const [error, setError] = useState(false);
 
-  useEffect(() => {
-    axios
-      .get("/stories")
-      .then((res) => {
-        setStoryArr(res.data);
-      })
-      .catch((err) => console.log(err));
-  }, []);
+    useEffect(() => {
+        axios
+            .get('/stories')
+            .then((res) => {
+                setStoryArr(res.data);
+            })
+            .catch((err) => {
+                if (err.response) {
+                    console.log(err);
+                    setError(!error)
+                }
+            });
+    }, []);
 
-  const handleClick = (e) => {
-    const id = e.target.dataset.id;
-    const name = e.target.dataset.name;
-    setStoryId(id);
-    setStoryName(name);
-  };
+    const handleClick = (e) => {
+        const id = e.target.dataset.id;
+        const name = e.target.dataset.name;
+        setStoryId(id);
+        setStoryName(name);
+    };
 
-  return (
-    <>
-      {storyArr.map((story) => {
-        return (
-          <Link to={`/story/${story.storyId}`} className="story-link">
-            <div className='block'>
-              <h2
-                className="story-name"
-                key={story.storyId}
-                onClick={handleClick}
-                data-id={story.storyId}
-                data-name={story.name}
-              >
-                {story.name}
-              </h2>
-            </div>
-          </Link>
-        );
-      })}
-    </>
-  );
+    return (
+        <>
+            {error ? (
+                <Error />
+            ) : (
+                storyArr.map((story) => {
+                    return (
+                        <Link
+                            to={`/story/${story.storyId}`}
+                            className='story-link'>
+                            <div className='block'>
+                                <h2
+                                    className='story-name'
+                                    key={story.storyId}
+                                    onClick={handleClick}
+                                    data-id={story.storyId}
+                                    data-name={story.name}>
+                                    {story.name}
+                                </h2>
+                            </div>
+                        </Link>
+                    );
+                })
+            )}
+        </>
+    );
 };
 
 const mapDispatchToProps = (dispatch) => ({
-  setStoryId: (id) => dispatch(setStoryId(id)),
-  setStoryName: (name) => dispatch(setStoryName(name)),
+    setStoryId: (id) => dispatch(setStoryId(id)),
+    setStoryName: (name) => dispatch(setStoryName(name))
 });
 
 export default connect(null, mapDispatchToProps)(Home);
